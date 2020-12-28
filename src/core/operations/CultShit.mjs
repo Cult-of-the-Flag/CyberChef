cceadad447e92b600fde821a0c8e1e6b0e839573
Ata <ahakcil@gmail.com>
2020-12-29 00:35:27 +0300

    Remove debug stuff from secret recipe

--- src/core/operations/CultShit.mjs
+++ src/core/operations/CultShit.mjs
@@ -6,6 +6,7 @@ import Operation from "../Operation.mjs";
 import XORplain from "./XorKnownPlain.mjs";
 import ROT13 from "./ROT13.mjs";
 import ROT47 from "./ROT47.mjs";
+import FromBase91 from "./FromBase91.mjs";
 
 class CultMagic extends Operation {
 
@@ -38,23 +39,29 @@ class CultMagic extends Operation {
      * @returns {byteArray}
      */
     run(input, args) {
-        console.log(input)
         const alphabet = /^[ -~]+$/;
         const crib = args[0]
         const format = RegExp("^" + args[0].replace('{', '\\{').replace('}', '\\}') + "$")
+        
         var x = String.fromCharCode.apply(String, new Uint8Array(input))
         if(this._correct(x,format,alphabet)){
             return this.str2arr("This is a fucking flag you dipshit.")
         }
 
+        x = FromBase91.call(input, [true])
+        x = String.fromCharCode.apply(String, new Uint8Array(x))
+        if(this._correct(x,format,alphabet)){
+            return this.str2arr("This is base91. Your fucking flag is: \n" + x)
+        }
+
         for(var i=1; i<26; i++){
-            x = new ROT13().run(input, [true,true,i])
+            x = ROT13.call(input, [true,true,i])
             x = String.fromCharCode.apply(null, new Uint8Array(x))
             if(this._correct(x,format,alphabet)){
                 return this.str2arr("This is rot13 with offset: " + String(i) + ". Your fucking flag is: \n" + x)
             }
         }
-        
+
         for(var i=1; i<26; i++){
             var x = ROT13.call(input, [false,true,i])
             x = String.fromCharCode.apply(String, new Uint8Array(x))
@@ -66,8 +73,6 @@ class CultMagic extends Operation {
         for(var i=1; i<26; i++){
             var x = ROT13.call(input, [true,false,i])
             x = String.fromCharCode.apply(String, new Uint8Array(x))
-            console.log("rot13ed:")
-            console.log(x)
             if(this._correct(x,format,alphabet)){
                 return this.str2arr("This is rot13 with lowercase only offset: " + String(i) + ". Your fucking flag is: \n" + x)
             }
@@ -76,7 +81,6 @@ class CultMagic extends Operation {
         for(var i=1; i<92; i++){
             var x = ROT47.call(input, [i])
             x = String.fromCharCode.apply(String, new Uint8Array(x))
-            console.log("rot47ed:")
             if(this._correct(x,format,alphabet)){
                 return this.str2arr("This is rot47 with offset: " + String(i) + ". Your fucking flag is: \n" + x)
             }
@@ -85,7 +89,6 @@ class CultMagic extends Operation {
         for(var i=0;i<crib.length;i++){
             var x = XORplain.call(input, [{option:"latin1", string:crib.slice(0,i)}, "Standart", false])
             x = String.fromCharCode.apply(String, new Uint8Array(x))
-            console.log(crib.slice(0,i))
             if(this._correct(x,format,alphabet)){
                 return this.str2arr("This is XOR with a " + String(i) + " length key. Your fucking flag is: \n" + x)
             }
@@ -103,7 +106,6 @@ class CultMagic extends Operation {
       }
     
     _correct(str, format, alphabet) {
-        console.log(str)
         if(str.match(format) && str.match(alphabet)){
             return true;
         }
